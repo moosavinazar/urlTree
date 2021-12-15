8b46adb53cffa9f5dc25d81d83b6e3fdd46d25d2
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-15 17:16:18 +0330

    urlTree priority

--- src/app/limited-access.guard.ts
+++ src/app/limited-access.guard.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LimitedAccessGuard implements CanActivate {
+
+  constructor(private router: Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    // if (condition) {
+    //   return true;
+    // }
+    return this.router.createUrlTree(['/limited-access'])
   }
-  
+
 }
