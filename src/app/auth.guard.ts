87253f362a33c8cba99039e0075c93b6664b9165
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-15 17:30:42 +0330

    use state and set return url

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private router:Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    let roles = route.data['roles'] as string[];
+    // let roles = route.data['roles'] as string[];
 
     // if (localstorage.getItem('user') or roles.indexOf(user.role) <= -1) {
     //   return true;
     // }
 
-    return true;
+    let returnUrl = state.url;
+    return this.router.createUrlTree(['/not-authorized'], {
+      queryParams: {
+        returnUrl: returnUrl
+      }
+    });
+
+    //return true;
   }
 
 }
